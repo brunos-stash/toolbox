c0f6d8dbe70e7c87f2e092327cd5dbfbbbf9d656
brunos-stash <topkekmp4@gmail.com>
2019-09-19 21:42:45 +0200

    updated to version 0.1.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/brunos-stash/toolbox'
 EMAIL = 'topkekmp4@gmail.com'
 AUTHOR = 'Tony Zero'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -55,43 +55,6 @@ else:
     about['__version__'] = VERSION
 
 
-# class UploadCommand(Command):
-#     """Support setup.py upload."""
-
-#     description = 'Build and publish the package.'
-#     user_options = []
-
-#     @staticmethod
-#     def status(s):
-#         """Prints things in bold."""
-#         print('\033[1m{0}\033[0m'.format(s))
-
-#     def initialize_options(self):
-#         pass
-
-#     def finalize_options(self):
-#         pass
-
-#     def run(self):
-#         try:
-#             self.status('Removing previous builds…')
-#             rmtree(os.path.join(here, 'dist'))
-#         except OSError:
-#             pass
-
-#         self.status('Building Source and Wheel (universal) distribution…')
-#         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-#         self.status('Uploading the package to PyPI via Twine…')
-#         os.system('twine upload dist/*')
-
-#         self.status('Pushing git tags…')
-#         os.system('git tag v{0}'.format(about['__version__']))
-#         os.system('git push --tags')
-
-#         sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -103,9 +66,9 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['toolbox.web'],
+    # py_modules=['toolbox'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
@@ -125,8 +88,4 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    # $ setup.py publish support.
-    # cmdclass={
-    #     'upload': UploadCommand,
-    # },
 )
